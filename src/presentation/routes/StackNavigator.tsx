d9dffd9294094a9e6adffcb33ab140d6e96b65af
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-13 11:05:46 -0600

    Pantalla de productos

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {ProductsScreen} from '../screens/products/ProductsScreen';
 import {SettingsScreen} from '../screens/settings/SettingsScreen';
 import {HomeScreen} from '../screens/home/HomeScreen';
+import {ProductScreen} from '../screens/products/ProductScreen';
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,7 @@ export const StackNavigator = () => {
       }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
