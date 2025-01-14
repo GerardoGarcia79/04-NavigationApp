8de801acd5d2766e70836dd95a9c01cfd19dd8a6
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-14 17:25:49 -0600

    Toggle Drawer - Mostrar/Ocultar

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,29 @@
-import {NavigationProp, useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {View} from 'react-native';
+import {
+  DrawerActions,
+  NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Pressable, Text, View} from 'react-native';
 import {PrimaryButton} from '../../../components/shared/PrimaryButton';
 import {globalStyles} from '../../theme/theme';
 import {RootStackParams} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      // eslint-disable-next-line react/no-unstable-nested-components
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>
+      ),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <View style={globalStyles.container}>
