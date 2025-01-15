6b5d452875e90c38b1d354cc34aee094aff8c73a
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-15 08:51:50 -0600

    Menú de hamburguesa

---
+++ src/components/shared/HamburgerMenu.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useNavigation, DrawerActions} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Pressable, Text} from 'react-native';
+
+export const HamburgerMenu = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menú</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
+
+  return <></>;
+};
